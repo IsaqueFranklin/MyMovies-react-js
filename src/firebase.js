f3f3ccfd2e32bbd21a7b2269da94da8ccdd20a56
Isaque F <isaquefrankli@gmail.com>
2021-04-29 10:28:47 -0300

    ops forgot to remove my firebase tokens

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import firestore from 'firebase/firestore'
 import React from 'react';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjrsmyPExeQCFr33YKF3hLcM-b58RrbsE",
-  authDomain: "todoapp-10663.firebaseapp.com",
-  databaseURL: "https://todoapp-10663.firebaseio.com",
-  projectId: "todoapp-10663",
-  storageBucket: "todoapp-10663.appspot.com",
-  messagingSenderId: "49063263870",
-  appId: "1:49063263870:web:6b7dc6eac9f875fcb1622f"
+  
 };
   // Initialize Firebase
   const fb = firebase.initializeApp(firebaseConfig);
